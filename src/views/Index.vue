83a347677652332c4216aeb3448ae816db7d421d
jsmolka <jsmolka@pm.me>
2024-08-10 23:13:45 +0200

    cleanup

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -30,8 +30,8 @@ import { CubeEdgesGeometry } from '@/graphic/cubeEdgesGeometry';
 import { CubeGeometry } from '@/graphic/cubeGeometry';
 import { dispose } from '@/graphic/dispose';
 import { Graphic } from '@/graphic/graphic';
+import { InstancedMesh } from '@/graphic/instancedMesh';
 import { InstancedPositionLineSegments } from '@/graphic/instancedPositionLineSegments';
-import { InstancedPositionMesh } from '@/graphic/instancedPositionMesh';
 import { Maze } from '@/modules/maze';
 import { neighbor } from '@/modules/neighbor';
 import { RecursiveBacktracking } from '@/modules/recursiveBacktracking';
@@ -45,6 +45,10 @@ import { onBeforeUnmount, onMounted, ref, watch } from 'vue';
 const { settings } = storeToRefs(useSettingsStore());
 
 const container = ref();
+
+const xAxis = neighbor.px | neighbor.nx;
+const yAxis = neighbor.py | neighbor.ny;
+const zAxis = neighbor.pz | neighbor.nz;
 
 let maze = null;
 const initMaze = () => {
@@ -63,6 +67,8 @@ class MazeGraphic extends Graphic {
   constructor(container, options = {}) {
     super(container, options);
     this.setup();
+
+    this.edgesPositions = Array.from(Array(1 << 6), () => []);
   }
 
   setup() {
@@ -74,7 +80,7 @@ class MazeGraphic extends Graphic {
     this.group.translateZ(-maze.dimensions / 2 + 1 / 2);
     this.scene.add(this.group);
 
-    this.cubes = new InstancedPositionMesh(
+    this.cubes = new InstancedMesh(
       new CubeGeometry(),
       new MeshBasicMaterial({ color: colors.brand3.int }),
       maze.elements + maze.connectors,
@@ -95,50 +101,43 @@ class MazeGraphic extends Graphic {
   paint() {
     console.time('paint');
 
-    const xAxis = neighbor.px | neighbor.nx;
-    const yAxis = neighbor.py | neighbor.ny;
-    const zAxis = neighbor.pz | neighbor.nz;
-
-    const edgesPositions = Array.from(Array(1 << 6), () => []);
+    for (const positions of this.edgesPositions) {
+      positions.length = 0;
+    }
 
     let i = 0;
     let count = 0;
-    for (let z = 0; z < maze.size; z++) {
-      for (let y = 0; y < maze.size; y++) {
-        for (let x = 0; x < maze.size; x++) {
+    for (let z = 0; z < maze.dimensions; z += 2) {
+      for (let y = 0; y < maze.dimensions; y += 2) {
+        for (let x = 0; x < maze.dimensions; x += 2) {
           const neighbors = maze[i++];
           if (neighbors === 0) {
             continue;
           }
 
-          const cx = 2 * x;
-          const cy = 2 * y;
-          const cz = 2 * z;
-
-          this.cubes.setPositionAt(count++, cx, cy, cz);
-          edgesPositions[neighbors].push(cx, cy, cz);
+          this.cubes.setPositionAt(count++, x, y, z);
+          this.edgesPositions[neighbors].push(x, y, z);
 
           if (neighbors & neighbor.px) {
-            this.cubes.setPositionAt(count++, cx + 1, cy, cz);
-            edgesPositions[xAxis].push(cx + 1, cy, cz);
+            this.cubes.setPositionAt(count++, x + 1, y, z);
+            this.edgesPositions[xAxis].push(x + 1, y, z);
           }
           if (neighbors & neighbor.py) {
-            this.cubes.setPositionAt(count++, cx, cy + 1, cz);
-            edgesPositions[yAxis].push(cx, cy + 1, cz);
+            this.cubes.setPositionAt(count++, x, y + 1, z);
+            this.edgesPositions[yAxis].push(x, y + 1, z);
           }
           if (neighbors & neighbor.pz) {
-            this.cubes.setPositionAt(count++, cx, cy, cz + 1);
-            edgesPositions[zAxis].push(cx, cy, cz + 1);
+            this.cubes.setPositionAt(count++, x, y, z + 1);
+            this.edgesPositions[zAxis].push(x, y, z + 1);
           }
         }
       }
     }
     this.cubes.count = count;
-    this.cubes.positionAttribute.needsUpdate = true;
+    this.cubes.matrixAttribute.needsUpdate = true;
 
     dispose(this.edges);
-
-    for (const [neighborMask, positions] of edgesPositions.entries()) {
+    for (const [neighborMask, positions] of this.edgesPositions.entries()) {
       if (positions.length === 0) {
         continue;
       }
